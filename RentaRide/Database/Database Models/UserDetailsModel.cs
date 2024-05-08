c526b4e4c281ef0fc47da8811a729360c3ec8dbe
Luis Rimando <alfonsorimando15@gmail.com>
2024-05-08 23:53:43 +0800

    First Push -Identity DB initialized -Registration user details initialized

---
+++ RentaRide/Database/Database Models/UserDetailsModel.cs
@@ -0,0 +1,34 @@
+﻿using RentaRide.Models.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentaRide.Database.Database_Models
+{
+    public class UserDetailsModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [ForeignKey("UserID")]
+        public RentaRideAppUsers RentaRideAppUsers { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string? StreetAdd { get; set; }
+        [Required]
+        public string? CityAdd { get; set; }
+        [Required]
+        public string? ProvinceAdd { get; set; }
+        [Required]
+        public string? Contact { get; set; }
+        //[Required]
+        //public string? License { get; set; }
+        //[Required]
+        //public string? 2ndValidID { get; set; }
+        //[Required]
+        //public string? PlaceOfBirth { get; set; }
+        //[Required]
+        //public string? SelfieProof { get; set; }
+    }
+}
---
+++ RentaRide/Migrations/20240508150829_RARAppUsersTBL.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentaRide.Migrations
+{
+    /// <inheritdoc />
+    public partial class RARAppUsersTBL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "AspNetUsers");
+        }
+    }
+}
