9c625ba52a07d028d9aada53c615017fa8a8d831
Jason Gorospe <9.pjgorospe@gmail.com>
2024-05-08 11:17:53 +0800

    Frontend for Login and Registration (not yet done)

---
+++ RentaRide/wwwroot/js/registration/registration.js
@@ -0,0 +1,16 @@
+﻿//document.addEventListener("DOMContentLoaded", function () {
+//    const forms = document.querySelectorAll("#moduleRegistration .form-container");
+//    let currentFormIndex = 0;
+
+//    // Function to handle form switching
+//    function showNextForm() {
+//        forms[currentFormIndex].style.display = "none";
+//        currentFormIndex++;
+//        forms[currentFormIndex].style.display = "block";
+//    }
+
+//    // Add event listeners to the continue buttons
+//    document.querySelectorAll("#moduleRegistration .btn-primary").forEach(button => {
+//        button.addEventListener("click", showNextForm);
+//    });
+//});
