c243c21265c3d89f8dfb439ca18b127091e62660
Jason Gorospe <9.pjgorospe@gmail.com>
2024-05-11 00:41:39 +0800

    Customer and admin home

---
+++ RentaRide/Controllers/CustomerHome.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentaRide.Controllers
+{
+    public class CustomerHome : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
