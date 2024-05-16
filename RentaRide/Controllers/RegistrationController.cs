125d44bb4f05a8b1af905e18c41ade572e6fa9a8
Luis Rimando <alfonsorimando15@gmail.com>
2024-05-16 20:22:54 +0800

    minor minor fix

--- RentaRide/Controllers/RegistrationController.cs
+++ RentaRide/Controllers/RegistrationController.cs
@@ -114,7 +114,8 @@ namespace RentaRide.Controllers
             return View(model);
         }
 
-        public string ProcessUploadedFile(IFormFile img, string imgCategory, string UID)
+        [NonAction]
+        private string ProcessUploadedFile(IFormFile img, string imgCategory, string UID)
         {
             string uniqueFileName = null;
             string UploadFolder = Path.Combine(FileLoc.FileUploadFolder, imgCategory);
@@ -137,7 +138,8 @@ namespace RentaRide.Controllers
             return uniqueFileName;
         }
 
-        public string GetFileExtension(IFormFile img)
+        [NonAction]
+        private string GetFileExtension(IFormFile img)
         {
             string fileExtension = null;
             if (img != null)
