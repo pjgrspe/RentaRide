9193c634ff79f95e1b0cb97013e957bfb9862b99
Jason Gorospe <9.pjgorospe@gmail.com>
2024-05-27 00:01:02 +0800

    Add car modal, separated js files, styling fixes

---
+++ RentaRide/wwwroot/js/admin/drivers.js
@@ -0,0 +1,183 @@
+﻿/* ---------------------------------------------------
+    DRIVERS TAB SCRIPT
+----------------------------------------------------- */
+
+/*ADD DRIVER MODAL*/
+function openModalAddDriver() {
+    $('#addNewDriverModal').modal('show');
+}
+
+function closeModalAddDriver() {
+    $('#addNewDriverModal').modal('hide');
+}
+
+function addDriver() {
+    var formData = new FormData();
+    formData.append('drivmodelFirstName', $('#driverFirstName').val());
+    formData.append('drivmodelMiddleName', $('#driverMiddleName').val());
+    formData.append('drivmodelLastName', $('#driverLastName').val());
+    formData.append('drivmodelEmail', $('#driverEmail').val());
+    formData.append('drivmodelContact', $('#driverContact').val());
+    formData.append('drivmodelImage', $('#driverPicture')[0].files[0]);
+    formData.append('drivmodelLicense', $('#driverLicenseFront')[0].files[0]);
+    formData.append('drivmodelLicenseBack', $('#driverLicenseBack')[0].files[0]);
+
+    fetch('/Admin/AddNewDriver', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text().then(text => text ? JSON.parse(text) : {}))
+        .then(data => {
+            if (data.success) {
+                reloadActivePartialView("Driver successfully added.");
+            } else {
+                reloadActivePartialView("Something went wrong.");
+            }
+            closeModalAddDriver();
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+/*EDIT DRIVER MODAL*/
+function openModalEditDriver(id, firstname, middlename, lastname, email, contact, status) {
+    $('#driverID-edit').val(id);
+    $('#driverFirstName-edit').val(firstname);
+    $('#driverMiddleName-edit').val(middlename);
+    $('#driverLastName-edit').val(lastname);
+    $('#driverEmail-edit').val(email);
+    $('#driverContact-edit').val(contact);
+    $('#driverStatus-edit').val(status);
+
+    $('#editDriverModal').modal('show');
+}
+
+function closeModalEditDriver() {
+    $('#driverID-edit').val('');
+    $('#driverFirstName-edit').val('');
+    $('#driverMiddleName-edit').val('');
+    $('#driverLastName-edit').val('');
+    $('#driverEmail-edit').val('');
+    $('#driverContact-edit').val('');
+    $('#driverStatus-edit').val('');
+
+    $('#editDriverModal').modal('hide');
+}
+
+function editDriver() {
+    var formData = new FormData();
+    formData.append('driveditmodelID', $('#driverID-edit').val());
+    formData.append('driveditmodelFirstName', $('#driverFirstName-edit').val());
+    formData.append('driveditmodelMiddleName', $('#driverMiddleName-edit').val());
+    formData.append('driveditmodelLastName', $('#driverLastName-edit').val());
+    formData.append('driveditmodelEmail', $('#driverEmail-edit').val());
+    formData.append('driveditmodelContact', $('#driverContact-edit').val());
+    formData.append('driveditmodelStatus', $('#driverStatus-edit').val());
+    formData.append('driveditmodelImage', $('#driverPicture-edit')[0].files[0]);
+    formData.append('driveditmodelLicense', $('#driverLicenseFront-edit')[0].files[0]);
+    formData.append('driveditmodelLicenseBack', $('#driverLicenseBack-edit')[0].files[0]);
+
+    fetch('/Admin/EditDriver', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text().then(text => text ? JSON.parse(text) : {}))
+        .then(data => {
+            //JASON INSERT YOUR JS CODE HERE
+            if (data.success) {
+                //SUCCESS
+                reloadActivePartialView("Driver successfully edited.");
+            } else {
+                //FAILED
+                reloadActivePartialView("Something went wrong.");
+                //alert(data.message); <-- USE THIS IF YOU WANT TO DISPLAY THE MESSAGE FROM THE CONTROLLER
+            }
+            closeModalEditDriver();
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+/*DELETE DRIVER MODAL*/
+function openModalDeleteDriver(driverFullname, driverID) {
+    $('#driverID-delete').val(driverID);
+
+    // Set the driver's full name in the modal
+    $('#deleteDriverName').text(`${driverFullname}?`);
+
+    $('#deleteDriverModal').modal('show');
+}
+function closeModalDeleteDriver() {
+    $('#driverID-delete').val('');
+    $('#deleteDriverName').text('');
+
+    $('#deleteDriverModal').modal('hide');
+}
+
+function deleteDriver() {
+    var formData = new FormData();
+    formData.append('drivdelmodelID', $('#driverID-delete').val());
+
+    fetch('/Admin/DeleteDriver', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text().then(text => text ? JSON.parse(text) : {}))
+        .then(data => {
+            //JASON INSERT YOUR JS CODE HERE
+            if (data.success) {
+                //SUCCESS
+                reloadActivePartialView("Driver successfully deleted.");
+            } else {
+                //FAILED
+                reloadActivePartialView("Something went wrong.");
+                //alert(data.message); <-- USE THIS IF YOU WANT TO DISPLAY THE MESSAGE FROM THE CONTROLLER
+            }
+            closeModalDeleteDriver()
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+/*CONTACT MODAL*/
+function openModalDriverContact(name, email, contact) {
+    $('#modalUserName').text(name);
+    $('#modalUserEmail').text(email);
+    $('#modalUserContact').text(contact);
+
+    $('#contactModal').modal('show');
+}
+
+function closeModalDriverContact() {
+    $('#modalUserName').text('');
+    $('#modalUserEmail').text('');
+    $('#modalUserContact').text('');
+    $('#contactModal').modal('hide');
+}
+
+
+
+/*DRIVER PICTURE MODAL*/
+function openModalDriverPicture(driverPicture) {
+    $('#driversPictureModal img').attr('src', driverPicture);
+
+    $('#driversPictureModal').modal('show');
+}
+
+function closeModalDriverPicture() {
+    $('#driversPictureModal img').attr('src', '');
+    $('#driversPictureModal').modal('hide');
+}
+
+
+
+/*VIEW DRIVER'S LICENSE MODAL*/
+function openModalViewDriverLicense(front, back) {
+    $('#driversLicenseFrontImage').attr('src', front);
+    $('#driversLicenseBackImage').attr('src', back);
+
+    $('#driversLicenseModal').modal('show');
+}
+
+function closeModalViewDriverLicense() {
+    $('#driversLicenseFrontImage').attr('src', '');
+    $('#driversLicenseBackImage').attr('src', '');
+    $('#driversLicenseModal').modal('hide');
+}
---
+++ RentaRide/wwwroot/js/admin/users.js
@@ -0,0 +1,98 @@
+﻿/* ---------------------------------------------------
+    USER TAB SCRIPTS
+----------------------------------------------------- */
+function openModalUserDetails(id, name, email, contact, status, dateCreated, dateModified, dateOfBirth, streetAddress, city, province, driversLicenseSrc, driversLicenseBackSrc, secondaryIDSrc, proofOfBillingSrc, selfiePicSrc) {
+    $('#modalUserId').text(id);
+    $('#modalUserName').text(name);
+    $('#modalUserEmail').text(email);
+    $('#modalUserContact').text(contact);
+    $('#modalUserStatus').text(status);
+
+    // Set additional modal data
+    $('#modalDateCreated').text(dateCreated);
+    $('#modalDateModified').text(dateModified);
+    $('#modalDateOfBirth').text(dateOfBirth);
+    $('#modalStreetAddress').text(streetAddress);
+    $('#modalCity').text(city);
+    $('#modalProvince').text(province);
+
+    // Set the src attribute for the images
+    $('#modalDriversLicense img').attr('src', driversLicenseSrc);
+    $('#driversLicenseFrontImage').attr('src', driversLicenseSrc);
+    $('#driversLicenseBackImage').attr('src', driversLicenseBackSrc);
+    $('#modalSecondaryID img').attr('src', secondaryIDSrc);
+    $('#secondaryIDModal img').attr('src', secondaryIDSrc);
+    $('#modalProofOfBilling img').attr('src', proofOfBillingSrc);
+    $('#proofOfBillingModal img').attr('src', proofOfBillingSrc);
+    $('#modalSelfieWithID img').attr('src', selfiePicSrc);
+    $('#selfieWithIDModal img').attr('src', selfiePicSrc);
+    document.getElementById('userID-Verify').value = id;
+
+    $('#userModal').modal('show');
+}
+
+function closeModalUserDetails() {
+    $('#modalUserId').text('');
+    $('#modalUserName').text('');
+    $('#modalUserEmail').text('');
+    $('#modalUserContact').text('');
+    $('#modalUserStatus').text('');
+
+    // Clear additional modal data
+    $('#modalDateCreated').text('');
+    $('#modalDateModified').text('');
+    $('#modalDateOfBirth').text('');
+    $('#modalStreetAddress').text('');
+    $('#modalCity').text('');
+    $('#modalProvince').text('');
+
+    // Clear the src attribute for the images
+    $('#modalDriversLicense img').attr('src', '');
+    $('#driversLicenseFrontImage').attr('src', '');
+    $('#driversLicenseBackImage').attr('src', '');
+    $('#modalSecondaryID img').attr('src', '');
+    $('#secondaryIDModal img').attr('src', '');
+    $('#modalProofOfBilling img').attr('src', '');
+    $('#proofOfBillingModal img').attr('src', '');
+    $('#modalSelfieWithID img').attr('src', '');
+    $('#selfieWithIDModal img').attr('src', '');
+    $('#userID-Verify').text('');
+
+    $('#userModal').modal('hide');
+}
+
+function setUserVerify(isVerified) {
+    var formData = new FormData();
+    formData.append('userverID', $('#userID-Verify').val());
+    formData.append('userverIsVerified', isVerified);
+
+    fetch('/Admin/UserVerify', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text().then(text => text ? JSON.parse(text) : {}))
+        .then(data => {
+            if (data.success) {
+                //SUCCESS
+                if (isVerified == true) {
+                    //JASON INSERT YOUR JS CODE HERE
+                    reloadActivePartialView("User approved.");
+                }
+                else if (isVerified == false) {
+                    //JASON INSERT YOUR JS CODE HERE
+                    reloadActivePartialView("User denied.");
+                }
+                else {
+                    //JASON INSERT YOUR JS CODE HERE
+                    reloadActivePartialView("User set to Pending.");
+                }
+            } else {
+                //FAILED
+                //reloadActivePartialView("Something went wrong.");
+                //alert(data.message); <-- USE THIS IF YOU WANT TO DISPLAY THE MESSAGE FROM THE CONTROLLER
+                alert("Something went wrong");
+            }
+            closeModalUserDetails();
+        })
+        .catch(error => console.error('Error:', error));
+}
