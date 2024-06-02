ffeeb84cbfd8ad28e7a02b9baba9c8577c018248
Luis Rimando <alfonsorimando15@gmail.com>
2024-06-02 12:52:38 +0800

    Details backend, initial logs function and table (new migration for now, no new db backup file)
    -details frontend display fix needed (SAVE ME JASON SAVE ME)

---
+++ RentaRide/Models/Accounts/CarAddLogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RentaRide.Models.Accounts
+{
+    public class CarAddLogModel
+    {
+        public string addLogDetails { get; set; }
+        public bool? addLogType { get; set; }
+        public int addLogMileage { get; set; }
+        public DateTime addLogDate { get; set; }
+    }
+}
