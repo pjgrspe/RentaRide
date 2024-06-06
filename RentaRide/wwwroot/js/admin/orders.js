17387b7e8811e246d3b02e3893459ba46fe88804
Jason Gorospe <9.pjgorospe@gmail.com>
2024-06-07 00:45:10 +0800

    flatpickr orders tab

--- RentaRide/wwwroot/js/admin/orders.js
+++ RentaRide/wwwroot/js/admin/orders.js
@@ -1,5 +1,26 @@
 ﻿function openModalAddOrder() {
     $('#addOrderModal').modal('show');
+    // Calculate the date 18 years ago from today
+    const today = new Date();
+
+    // Initialize Flatpickr for the "Order Start Date" and "Order End Date" fields
+    flatpickr(".flatpickr-orderstart", {
+        dateFormat: "Y-m-dTH:i",
+        enableTime: true,
+        altInput: true,
+        altFormat: "F j, Y, H:i",
+        allowInput: true,
+        defaultDate: "today",
+    });
+
+    flatpickr(".flatpickr-orderend", {
+        dateFormat: "Y-m-dTH:i",
+        enableTime: true,
+        altInput: true,
+        altFormat: "F j, Y, H:i",
+        allowInput: true,
+        defaultDate: "today",
+    });
 }
 
 function closeModalAddOrder() {
